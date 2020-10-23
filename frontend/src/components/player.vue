87b67164ff9433c4053b3f84b6c3f0946ca6fe3a
markanesko <markoberic94@gmail.com>
2020-10-23 16:39:31 +0200

    adds player component

---
+++ frontend/src/components/player.vue
@@ -0,0 +1,26 @@
+<template>
+    <audio ref="player" controls>
+    </audio>
+
+</template>
+
+<script>
+export default {
+    name: 'player',
+
+    props: {
+        track: {
+            type: String,
+            default: "",
+            required: true,
+        }
+    },
+
+    watch: {
+        track(value, oldValue) {
+            this.$refs.player.src = value
+            this.$refs.player.play()
+        }
+    },
+}
+</script>
