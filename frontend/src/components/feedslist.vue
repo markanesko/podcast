cc2c32b11947c978e836dd630afa563e4b5e7a24
markanesko <markoberic94@gmail.com>
2020-10-23 16:38:49 +0200

    adds feedslist component

---
+++ frontend/src/components/feedslist.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="col-md-4">
+        <div class="list-group" v-if="feeds != {}">
+            <li class="list-group-itme" v-for="feed in feeds" @click="select(feed.title)" :class="{active: feed.title === selectedFeedTitle}">
+                {{ getShortTitle(feed.title) }}
+            </li>
+        </div>
+
+        <div v-else> No Podcast urls found </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'feedslist',
+
+    props: {
+        feeds: {
+            type: Object,
+            default: {},
+            required: true,
+        },
+
+        selectedFeedTitle: {
+            type: String,
+            default: "",
+            required: true,
+        },
+
+    },
+
+    methods: {
+        select (name) {
+            this.$emit("on-feed-select", name)
+        },
+
+        getShortTitle (title) {
+            if (!title) return "(No title)"
+            if (title.length > 35) return `${title.substr(0, 35)}`
+            else return title
+        }
+    }
+
+}
+</script>
+
+<style>
+    li.list-group-item {
+        cursor: pointer !important;
+    }
+</style>
